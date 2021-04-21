d2834cc792ad6b82f0b35e415627457937f20a72
Rayan Altamimi <rayantaw@gmail.com>
2021-04-21 17:48:17 +0300

    Initial Commit
    One more try with pencils

---
+++ src/main/java/io/rynartisan/pencil/PencilApplication.java
@@ -0,0 +1,13 @@
+package io.rynartisan.pencil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PencilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PencilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rynartisan/pencil/PencilApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rynartisan.pencil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PencilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
